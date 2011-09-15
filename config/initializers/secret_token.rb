7d01344b93e7f88b789aedf57a57a8012be0e74e
philmill <phil@philmill.net>
2011-09-15 13:41:31 -0400

    Initial Rails 3.1.0 commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MustacheDemo::Application.config.secret_token = 'cf17c01e42373ca42118133f6f4937b05e91e7c7b9f98cc495fda2364922d3d33027760f7fbc5920728cc0e68c67730f3220ee07ce3634656cd3c8681bf994ca'
